6c9c6a4bef1e0867ef73e521aef7a1bf3e76847e
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-02 11:21:22 +0000

    post request added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,62 @@ const API_KEY = "AyKHNtrxNT9prYSzp1cctLQpsQk"
 const API_URL = "https://ci-jshint.herokuapp.com/api"          // saves typing url for each call to api
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));   // allow to trigger modals using js (create modal using single line of js)
 
+
 //add event listener to first button. then call getStatus function,(e),event object, is passed but not needed now but is good practice with handlers
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+//POST request
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));     //formdata is a js interface which grabs form data asan object in key pairs
+
+//TEST -entries method allows us to iterate through form data to ensure data was captured 
+    // for (let e of form.entries()){     
+    //     console.log(e);
+    // }
+
+    const response = await fetch(API_URL, {  //must await fetch, as it returns a promise
+        method: "POST",
+        headers: {
+                "Authorization": API_KEY,
+             },
+             body: form,                   
+        });
+//ABOVE-- how to send form data to api: because of the formData object we can add it to request.
+// it will make a POST request to the api, authorize it with the api key, and attach the form as the body of the request
+     
+        const data = await response.json();
+        //response is converted to json
+        // json method returns a promise, which must be awaited also
+        if (response.ok) {
+            //console.log(data);   //used to test first time round
+               displayErrors(data);
+        } else {
+            throw new Error(data.error);
+        }
+}
+
+
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`; //pickup file value from returned json
+    if (data.total_errors === 0) {
+        results = `<div class="no _errors">No Errors Reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>` //classes are added if styling is wanted later
+        for (let error of data.error_list) {    //names are coming from json that is returned- 
+            results += `<div>At line <span class="line">${error.line}</span>,`;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;   //pass in error text returned from json
+        }
+    }
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+};
+
+
+
+//GET request
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`  //this follows the "get" instruction from https://ci-jshint.herokuapp.com/
 
@@ -16,7 +69,7 @@ async function getStatus(e) {
     //if server returns code if 200, all is ok and ok property is true
     //console out server response
     if (response.ok) {
-        displayStatus(data.expiry);  //display data in the modal instead of console logging
+        displayStatus(data);  //display data in the modal instead of console logging
         //console.log(data) //can log data.expiry instead to only see date in console without the status code
     } else{
         throw new Error(data.error); //data.error is descriptive message from json that is returned
@@ -28,10 +81,12 @@ function displayStatus(data) {
 
     let heading = "API_Key status";
     let results = `<div>Your key is valid until</div>`
-    results += `<div class ="key-status">${data.expiry}</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
 
     document.getElementById("resultsModalTitle").innerHTML = heading;
-    Document.getElementById("results-content").innerHTML = results;
+    document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
-
 }
+
+
+
